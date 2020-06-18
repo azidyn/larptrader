c9a71520408a46f0c2a557e55dd435689451287f
alec azimuth <azidyn@protonmail.com>
2020-06-18 09:35:24 +0100

    add config

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    api: {
+        live: 'https://www.bitmex.com/',
+        testnet: 'https://testnet.bitmex.com/',
+    },  
+
+    key: {
+        live:    { id: '', secret: '' },   
+        testnet: { id: '', secret: '' }     // your api keys here
+    }
+
+}
