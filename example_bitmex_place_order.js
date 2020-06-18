858ff0ef79330047f6abf38142ce7a24f5830192
alec azimuth <azidyn@protonmail.com>
2020-06-18 12:13:44 +0100

    add place order example

---
+++ example_bitmex_place_order.js
@@ -0,0 +1,24 @@
+
+
+const BitMEX = require('./exchange/BitMEX');
+const config = require('./config');
+
+if ( !config.key.testnet.id )
+    console.warn(`No API key set in config.js`);
+
+// Note: you don't need a key for some bitmex functions
+let bitmex = new BitMEX({ livenet: false, id: config.key.testnet.id, secret: config.key.testnet.secret });
+
+(async()=>{
+
+    // Market close any open position
+    await bitmex.marketclose('XBTUSD');
+
+    // Market buy 1 contract
+    let result = await bitmex.marketorder( 'XBTUSD', 'Buy', 1 )
+
+    console.log( result );
+
+
+
+})();
